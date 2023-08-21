7a39a0de5cd3780173fafd07f3b970e89add0841
Muhammad Fadil Maulana Akbar <m.muhammadfadil12@gmail.com>
2023-08-21 11:40:39 +0700

    add api

---
+++ api_function.py
@@ -0,0 +1,25 @@
+from typing import Optional
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class QueryInput(BaseModel):
+    query: str
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://localhost:8080",
+    "http://localhost:5000",
+] 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
--- query.py
+++ query.py
@@ -2,50 +2,90 @@ from rdflib import Graph, Literal, URIRef
 from rdflib.namespace import RDF
 
 def get_query(text):
+    g = Graph()
+    g.parse("linked_data.ttl", format="turtle")
     keywords = text.split()
 
-    # relation
-    relation = ""
-    if keywords[2][:2].lower() == "pn" or keywords[2].lower() == "prioritas" and keywords[3].lower() == "nasional":
-        relation = "ssrnpn"
-    elif keywords[2][:2].lower() == "pp" or keywords[2].lower() == "program" and keywords[3].lower() == "prioritas":
-        relation = "ssrnpp"
-        
-    # search pattern
-    search_pattern = " ".join(keywords[2:])
-    if "prioritas nasional" in search_pattern.lower():
-        search_pattern = search_pattern.replace("prioritas nasional", "pn")
-    if "program prioritas" in search_pattern.lower():
-        search_pattern = search_pattern.replace("program prioritas", "pp")
+    if keywords[0].lower() == "target":
+        # relation
+        relation = ""
+        if keywords[2][:2].lower() == "pn" or keywords[2].lower() == "prioritas" and keywords[3].lower() == "nasional":
+            relation = "ssrnpn"
+        elif keywords[2][:2].lower() == "pp" or keywords[2].lower() == "program" and keywords[3].lower() == "prioritas":
+            relation = "ssrnpp"
+            
+        # search pattern
+        search_pattern = " ".join(keywords[2:])
+        if "prioritas nasional" in search_pattern.lower():
+            search_pattern = search_pattern.replace("prioritas nasional", "pn")
+        if "program prioritas" in search_pattern.lower():
+            search_pattern = search_pattern.replace("program prioritas", "pp")
 
-    sparql_query = """
-    PREFIX table: <http://www.semanticweb.org/fadil/ontologies/2023/7/ldt#/>
-    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        # target year
+        target_year = " ".join(keywords[-1:])
 
-    SELECT ?indikator ?target
-    WHERE {
-        ?program rdfs:label ?programtitle .
-        ?sasaran ?relation ?program .
-        ?sasaran table:indikator ?temp .
-        ?temp rdfs:label ?indikator .
-        ?temp table:target ?target
-        FILTER (?programtitle = ?searchPattern)
-    }
-    """
+        sparql_query = """
+        PREFIX table: <http://www.semanticweb.org/fadil/ontologies/2023/7/ldt#/>
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+        SELECT ?subject ?object
+        WHERE {
+            ?program rdfs:label ?programtitle .
+            ?sasaran ?relation ?program .
+            ?sasaran table:indikator ?temp .
+            ?temp rdfs:label ?subject .
+            ?temp table:target ?object
+            FILTER (?programtitle = ?searchPattern)
+        }
+        """
+
+        relation_uri = URIRef(f"table:{relation}")
+        query = sparql_query.replace("?searchPattern", f"'{search_pattern}'").replace("?relation", f"{relation_uri}")
+        results = g.query(query)
+        for row in results:
+            target = row['object'].value
+            if target_year in target:
+                indikator = row['subject'].value
+                print(indikator, ", ", target)
+
+    elif keywords[0].lower() == "apa":
+        # relation
+        if keywords[3][:2].lower() == "pn" or keywords[3].lower() == "prioritas" and keywords[4].lower() == "nasional":
+            relation = "pn"
+        elif keywords[2][:3].lower() == "rkp" or keywords[3].lower() == "rencana" and keywords[4].lower() == "kerja" and keywords[5].lower() == "pemerintah":
+            relation = "penjabaran"
+
+        # search pattern
+        search_pattern = " ".join(keywords[3:])
+        if "prioritas nasional" in search_pattern.lower():
+            search_pattern = search_pattern.replace("prioritas nasional", "pn")
+        if "rencana kerja pemerintah" in search_pattern.lower():
+            search_pattern = search_pattern.replace("rencana kerja pemerintah", "rkp")
+
+        # target year
+        target_year = " ".join(keywords[-1:])
+
+        sparql_query = """
+        PREFIX table: <http://www.semanticweb.org/fadil/ontologies/2023/7/ldt#/>
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+        SELECT ?subject ?object
+        WHERE {
+            ?subject ?relation ?object .
+            FILTER (?object = ?searchPattern)
+        }
+        """
 
     relation_uri = URIRef(f"table:{relation}")
     query = sparql_query.replace("?searchPattern", f"'{search_pattern}'").replace("?relation", f"{relation_uri}")
-
-    return query
-
-def get_result(query):
-    g = Graph()
-    g.parse("linked_data.ttl", format="turtle")
-    
     results = g.query(query)
-
-    for row in results:
-        indikator = row['indikator'].value
-        target = row['target'].value
-        print(indikator,', ', target)
+ 
+    print(results)
+    # for row in results:
+    #     object = row['object'].value
+    #     if target_year in object and object not in seen_targets:
+    #         subject = row['subject'].value
+    #         print(subject, ", ", object)
+    #         seen_targets.add(object)
