14a4a892f13d4df549302c736108b348a2c64992
Muhammad Fadil Maulana Akbar <m.muhammadfadil12@gmail.com>
2023-08-21 15:26:22 +0700

    add index.html

--- api_function.py
+++ api_function.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from query import get_query
+from fastapi import Request
 
 app = FastAPI()
 
@@ -10,9 +11,8 @@ class QueryInput(BaseModel):
     query: str
 
 origins = [
-    "http://localhost:3000",
     "http://localhost:8000",
-] 
+    "http://127.0.0.1:5500",]
 
 app.add_middleware(
     CORSMiddleware,
@@ -26,11 +26,11 @@ app.add_middleware(
 def read_root():
     return {"error":"False", "message":"Welcome to the API"}
 
-@app.post("/query")
+@app.post("/execute-query")
 def execute_query(query_input: QueryInput):
     try:
         query = query_input.query
         result = get_query(query)
-        return {"error":"False", "message":result}
+        return {"error":"False", "message":str(result)}
     except Exception as e:
         return {"error":"True", "message":str(e)}
