a35275cdc25b16ccb8ad1f96aebf95e448a5d4b3
Muhammad Fadil Maulana Akbar <m.muhammadfadil12@gmail.com>
2023-08-29 15:00:19 +0700

    fix

--- backend/api_function.py
+++ backend/api_function.py
@@ -12,9 +12,7 @@ class QueryInput(BaseModel):
     query: str
     # class_name: str
 
-origins = [
-    "http://localhost:8000",
-    "http://127.0.0.1:5500",]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
